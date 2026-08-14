98a666a4407bf4bfa9b52600b8928b3cb8277f2d
mia <radekjanisbubu@gmail.com>
2026-08-15 00:15:57 +0200

    config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Replace these placeholders before deploying.
 window.IMAGEBOARD_CONFIG = {
-  SUPABASE_URL: "https://ahudxrvrtfrvpaaitpsv.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://ahudxrvrtfrvpaaitpsv.supabase.co/rest/v1/boards",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFodWR4cnZydGZydnBhYWl0cHN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY3MzA0NTQsImV4cCI6MjEwMjMwNjQ1NH0.yZz9EH2YqACUysb3cVOjnld7cxa9MquC841C8VdGHxI"
 };
